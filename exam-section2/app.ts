4bcc637809597e3af743255a104ea92c58a868b8
Sebastian Diaz <jsebasdc@gmail.com>
2019-01-22 14:50:23 -0500

    Final Section 2

---
+++ exam-section2/app.ts
@@ -0,0 +1,62 @@
+
+// Uso de Let y Const
+let nombre:string = "Ricardo Tapia";
+let edad:number = 23;
+
+const PERSONAJE = {
+  nombre: nombre,
+  edad: edad
+};
+
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+let batman:Batman = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+
+interface Batman{
+  nombre:string;
+  artesMarciales:string[];
+}
+// Convertir esta funcion a una funcion de flecha
+// function resultadoDoble( a, b ){
+//   return (a + b) * 2 
+// }
+
+let resultadoDobleV2 = (a:number,b:number) => (a+b)*2;
+
+
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, arma:string = "escudo", poder?:string ){
+  let mensaje;
+  if( poder ){
+     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  }else{
+     mensaje = nombre + " tiene un " + poder
+  }
+};
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class Rectangulo{
+  base:number;
+  altura:number;
+
+  calcularArea():number{
+    return this.base * this.altura;
+  }
+}
+
+let prueba = `Prueba`;
+
