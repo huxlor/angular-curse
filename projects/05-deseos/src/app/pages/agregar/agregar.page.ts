70403f60400eb90d3f20e500fee15159bf06eb29
Sebastian Diaz <jsebasdc@gmail.com>
2019-03-15 13:57:00 -0500

    Class 126

--- projects/05-deseos/src/app/pages/agregar/agregar.page.ts
+++ projects/05-deseos/src/app/pages/agregar/agregar.page.ts
@@ -38,4 +38,27 @@ export class AgregarPage implements OnInit {
       this.deseosService.guardarStorage();
   }
 
+  cambioCheck( item: ListaItem) {
+
+    const pendientes = this.lista.items.filter( itemData => !itemData.completado ).length;
+    // console.log({pendientes});
+
+    if ( pendientes === 0 ) {
+        this.lista.terminadaEn = new Date();
+        this.lista.terminada = true;
+    } else {
+        this.lista.terminadaEn = null;
+        this.lista.terminada = false;
+    }
+
+    this.deseosService.guardarStorage();
+
+    console.log(this.deseosService.listas);
+  }
+
+  borrar(i: number) {
+    this.lista.items.splice( i, 1 );
+    this.deseosService.guardarStorage();
+  }
+
 }
--- projects/05-deseos/src/app/pages/tab1/tab1.page.ts
+++ projects/05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 
 
 @Component({
@@ -53,4 +54,10 @@ export class Tab1Page {
 
     alert.present();
   }
+
+  listaSeleccionada( lista: Lista ) {
+    const idLista = lista.id;
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${ idLista }`);
+  }
+
 }
