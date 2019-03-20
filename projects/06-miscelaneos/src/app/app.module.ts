615738ce1fa80245070aae0086f2258433c66231
Sebastian Diaz <jsebasdc@gmail.com>
2019-03-20 13:55:52 -0500

    Class 140

--- projects/06-miscelaneos/src/app/app.module.ts
+++ projects/06-miscelaneos/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule
