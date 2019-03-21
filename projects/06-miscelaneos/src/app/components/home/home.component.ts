5028f8bd4a1e124a7adafbc1b52733962b9ddb93
Sebastian Diaz <jsebasdc@gmail.com>
2019-03-21 14:01:05 -0500

    Class 143

---
+++ projects/06-miscelaneos/src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <app-ng-style></app-ng-style>
+    <hr>
+    <app-css></app-css>
+    <hr>
+    <app-clases></app-clases>
+    <hr>
+    <p [appResaltado]="'yellow'">Angular Directives</p>
+    <hr>
+    <app-ng-switch></app-ng-switch>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
