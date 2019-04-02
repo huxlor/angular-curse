f3a4d042f24e9a7925a8baf7e31af73a775b6dcb
Sebastian Diaz <jsebasdc@gmail.com>
2019-04-02 13:48:45 -0500

    Class 181

--- projects/08-formularios/src/app/components/data/data.component.ts
+++ projects/08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
 
 
@@ -15,7 +15,7 @@ export class DataComponent {
   usuario: Object = {
     nombrecompleto: {
       nombre: 'Sebastian',
-      apellido: 'Herrera'
+      apellido: 'Diaz'
     },
     correo: 'jsebasdc@gmail.com'
   };
@@ -28,18 +28,46 @@ export class DataComponent {
           Validators.required,
           Validators.minLength(3)
         ]),
-        'apellido': new FormControl('', Validators.required),
+        'apellido': new FormControl('', [Validators.required,
+                                         this.noHerrera]),
       }),
 
       'correo': new FormControl('', [
                                     Validators.required,
                                     Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-                                  ])
+                                  ]),
+      'pasatiempos': new FormArray([
+            new FormControl('Correr', Validators.required)
+      ])
     });
+
+    // this.forma.setValue( this.usuario );
+
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    );
+  }
+
+  noHerrera( control: FormControl ): {[s: string]: boolean} {
+    if ( control.value === 'herrera' ) {
+      return{
+        noherrera: true
+      };
+    }
   }
 
   guardarCambios () {
     console.log(this.forma);
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: '',
+    //     apellido: '',
+    //   },
+    //   correo: ''
+    // });
   }
 
 }
