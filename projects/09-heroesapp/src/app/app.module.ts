af3117a64cb4928e352b58e03ec62b1a3a5ebd20
Sebastian Diaz <jsebasdc@gmail.com>
2019-04-04 13:52:43 -0500

    Class 191

--- projects/09-heroesapp/src/app/app.module.ts
+++ projects/09-heroesapp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+
+import { appRouting } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouting,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
