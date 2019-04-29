11ee2f179b75482aedc600f3f427c18debb86cf3
Sebastian Diaz <jsebasdc@gmail.com>
2019-04-29 13:56:38 -0500

    Class 206

---
+++ projects/10-firechat/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule} from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
+
+import { ChatService } from './providers/chat.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: FirestoreSettingsToken, useValue: {} },
+    ChatService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
